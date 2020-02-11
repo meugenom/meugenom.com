191092476b71219a120832a49ca100926e75745e
Eugen Morozov <meugenom@gmail.com>
2020-02-11 14:58:23 +0100

    added informaton to about me page
    added information to about me page

--- src/components/About.js
+++ src/components/About.js
@@ -8,13 +8,11 @@ class About {
             <div class="container">
                 <article>
                     <h1> About me</h1>
-                        <p>
-                            ...
-                        </p>
-                        <h1>Interesting</h1>
-                        <p>
-                            ...
-                        </p>
+                        <p>Nice to see you.</p>
+                        <p>My name is Eugen and I live in Frankfurt am Main in Deutschland.</p>
+                        <p>When I was little, I started interesting things which influenced my opinion. It was mathematik, philosophy, biology and history. And all the time I wanted to structure my knowledge to understand how can use algorithms in my real life.<p/>
+                        <p>My web-life began early, just me working on my own. And in later, I have received formal education at the university.</p>
+                        <p>Today I primarily create websites and also work with development both frontend and backend, specifically with JavaScript, Java and Python. I have a wide range of skills to apply my knowledge to more sorts of projects.</p>                     
                 </article>
             </div>    
         </main>                                    
