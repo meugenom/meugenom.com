206407b3f9429d0df4fa05acf7456bef834b27d2
Eugen Morozov <meugenom@gmail.com>
2021-01-22 19:09:34 +0100

    moved css files for frontend and reactjs components
    css map's files were moved to resources of the project, and component's files for frontend

--- src/main/resources/js/Config.tsx
+++ src/main/resources/js/Config.tsx
@@ -1,6 +1,6 @@
 const Config = {
   dir: './../../../../content/posts/',
-  token: '',
+  token: '0defaced2e55a5d3499f224c1a139bec1c9fc20b',
   months: [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec']
 }
 export default Config
