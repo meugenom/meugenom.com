c61a3e9585386138ae55418e9c8cee5b410e065d
Eugen Morozov <meugenom@gmail.com>
2021-01-24 23:06:07 +0100

    changed graphql queries and schemas
    some fixes for graphql queries and schemas

--- src/main/java/dev/eugenem/graphql/resolver/ArticlesQuery.java
+++ src/main/java/dev/eugenem/graphql/resolver/ArticlesQuery.java
@@ -3,22 +3,33 @@ package dev.eugenem.graphql.resolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-
 import dev.eugenem.article.model.Article;
 import dev.eugenem.article.ArticleRepository;
 
+
 import java.util.List;
+import java.util.Optional;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
-
 
 @Component
 public class ArticlesQuery implements GraphQLQueryResolver {
 
   @Autowired
-  private ArticleRepository articlesRepository;
+  private ArticleRepository articleRepository;
 
-  public List<Article> lastArticlesList() {    
-    return articlesRepository.findAll();
+  public List<Article> lastArticlesList() {
+    return  articleRepository.findAll();
+
   }
+
+  public Optional<Article> getArticleById(Long id){
+    return articleRepository.findById(id);
+  }
+  
+  public String getName(String name){
+    return "Hello "+ name;
+  }
+  
+  
 }
