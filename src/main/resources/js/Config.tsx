a75f3463661c0b6f793c1ac127389dea8b721780
Eugen Morozov <meugenom@gmail.com>
2021-01-30 23:25:50 +0100

    deleted developed token from configs
    deleted github developed token

--- src/main/resources/js/Config.tsx
+++ src/main/resources/js/Config.tsx
@@ -1,6 +1,6 @@
 const Config = {
   dir: './../../../../content/posts/',
-  token: 'de777d42039312538757c5e7d429ba7cb15777c3',
+  token: '',
   months: [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec']
 }
 export default Config
