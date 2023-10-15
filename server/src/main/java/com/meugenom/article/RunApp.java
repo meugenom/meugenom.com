3d877bd47f4e36e741e22444b71e7e27ef2aa6fd
Eugen Morozov <meugenom@gmail.com>
2023-10-15 17:22:56 +0200

    fixed backend code

--- server/src/main/java/com/meugenom/article/RunApp.java
+++ server/src/main/java/com/meugenom/article/RunApp.java
@@ -1,17 +1,9 @@
 package com.meugenom.article;
 
-/**
- * @author meugenom
- * reading files from directory "/content/articles"
- * parsing and saving data from files in the articleRepository
- */
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
@@ -19,11 +11,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Set;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import com.meugenom.article.model.Article;
 import com.meugenom.article.parser.ParseToArticle;
@@ -44,170 +32,92 @@ public class RunApp implements CommandLineRunner {
 	private boolean isExist = false;
 	private boolean isChanged = false;
 
-	
-			public void reloadArticles() {
+	public void reloadArticles() {
+		logger.info("Start loading articles from local files");
 
-			logger.info("Start loading articles from local files");
+		ReadFilenamesFromDirectory dirList = new ReadFilenamesFromDirectory();
+		Set<String> fileList = dirList.listFilesUsingJavaIO(articlesPath);
 
-			// get files list from folder 'content/posts/'
-			ReadFilenamesFromDirectory dirList = new ReadFilenamesFromDirectory();
-			
-			Set<String> fileList = dirList.listFilesUsingJavaIO(articlesPath);
+		logger.info("File list: {}", fileList);
 
-			logger.info("File list: {}", fileList);
+		logger.info("Read files: {}", fileList);
 
-			logger.info("Read files: {}", fileList);
+		id = 0;
 
-			id = 0;
-			
-			if(fileList.size() == 0){
-				
-				logger.warn("No files in directory {}", articlesPath);
-
-			}else{
-
+		if (fileList.isEmpty()) {
+			logger.warn("No files in directory {}", articlesPath);
+		} else {
 			for (String fileName : fileList) {
-
 				String filePath = articlesPath + "/" + fileName;
 
 				ReadFile readFile = new ReadFile();
 				String textFromFile;
 				
 				try {
-
 					isChanged = false;
 					isExist = false;
-					
 					textFromFile = readFile.read(filePath);
 					ParseToArticle parseToArticle = new ParseToArticle();
-
 					Article draft = parseToArticle.parse(textFromFile);
 					draft.setFileName(fileName);
 					draft.setId(id);
-
 					
-					//Create checksum for this file
-					File file = new File(filePath);
- 
-					//Use MD5 algorithm
-					MessageDigest md5Digest = MessageDigest.getInstance("MD5");
- 
-					//Get the checksum
-					String checksum = getFileChecksum(md5Digest, file);
- 
-					//see checksum
-					logger.info("File checksum : {}",checksum);
+					String checksum = getFileChecksum(filePath);
+					logger.info("File checksum : {}", checksum);
 					draft.setChecksum(checksum);
 
 					articleRepository.findAll().forEach(article -> {
-					
 						if (article.getId() == id) {
-							if(article.getChecksum() != checksum){
+							if (!article.getChecksum().equals(checksum)) {
 								isChanged = true;
 								isExist = true;
-							}else{
+							} else {
 								isExist = true;
 							}
 						}
 					});
 
-					//https://www.baeldung.com/spring-data-redis-tutorial
-
-					//doesn't exist
 					if (!isExist) {
 						logger.info("Add new article with ID = {}", id);
 						articleRepository.save(draft);
-					
-					//exists and has new version
-					} else if(isExist && isChanged){
+					} else if (isExist && isChanged) {
 						logger.info("Changed article with ID = {}", id);
 						articleRepository.deleteById(id);
 						articleRepository.save(draft);
-					//exists and don't has version
 					} else {
 						logger.warn("Article with ID = {} exists, hasn't new version", id);
-						//need change with new version
 					}
 
 					id++;
-
-			
-				} catch (IOException e) {
-				
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					
-				} catch (NoSuchAlgorithmException n) {
-				
-				// TODO Auto-generated catch block
-				
-				n.printStackTrace();
-			}
-			
+				} catch (IOException | NoSuchAlgorithmException e) {
+					logger.error("An error occurred: {}", e.getMessage());
+				}
 			}
 		}
 
-			logger.info("Number of articles is : {}", articleRepository.count());
-
+		logger.info("Number of articles is : {}", articleRepository.count());
 	}
-	
-	//start
+
 	@Override
 	public void run(String... args) throws Exception {
-
-		//reloadArticles();
-
-		//https://www.baeldung.com/java-timer-and-timertask
-		/*
-		TimerTask repeatedTask = new TimerTask() {
-			@Override
-			public void run() {
-				
-				reloadArticles();
-
-				Date date = new Date(); // This object contains the current date value
-				SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-				
-				logger.info("Article's List Reloaded, time: {}",formatter.format(date));
-			}};
-
-		Timer timer = new Timer("Timer");
-		
-		long delay = 0L;
-    	long period = 1000L * 60L * 20L; //* 24L; //every 20 min
-
-		timer.schedule(repeatedTask, 0, 1800000); 
-		*/
+		reloadArticles();
 	}
 
-	private static String getFileChecksum(MessageDigest digest, File file) throws IOException {
-		//Get file input stream for reading the file content
+	private static String getFileChecksum(String filePath) throws IOException, NoSuchAlgorithmException {
+		File file = new File(filePath);
+		MessageDigest digest = MessageDigest.getInstance("MD5");
 		FileInputStream fis = new FileInputStream(file);
-   
-  		//Create byte array to read data in chunks
-  		byte[] byteArray = new byte[1024];
-  		int bytesCount = 0; 
-    
-  		//Read file data and update in message digest
-  		while ((bytesCount = fis.read(byteArray)) != -1) {
-    		digest.update(byteArray, 0, bytesCount);
-  		};
-   
-  		//close the stream; We don't need it now.
-  		fis.close();
-   
-  		//Get the hash's bytes
-  		byte[] bytes = digest.digest();
-   
-  		//This bytes[] has bytes in decimal format;
-  		//Convert it to hexadecimal format
-  		StringBuilder sb = new StringBuilder();
-  		for(int i=0; i< bytes.length ;i++){
-    		sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-  		}
-   
-  		//return complete hash
-   		return sb.toString();
-}
-
+		byte[] byteArray = new byte[1024];
+		int bytesCount;
+		while ((bytesCount = fis.read(byteArray)) != -1) {
+			digest.update(byteArray, 0, bytesCount);
+		}
+		fis.close();
+		byte[] bytes = digest.digest();
+		StringBuilder sb = new StringBuilder();
+		for (byte aByte : bytes) {
+			sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
+		}
+		return sb.toString();
+	}
 }
--- server/start-prod-server.sh
+++ server/start-prod-server.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar target/react-and-spring-data-graphql-0.0.1-SNAPSHOT.jar
+java -jar target/spring-data-graphql-0.0.1-SNAPSHOT.jar
