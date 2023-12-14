c5bcebb2f8661793ad5ad27314822f1d04f6e0a2
Eugen Morozov <meugenom@gmail.com>
2023-12-14 19:47:53 +0100

    fixed configs
    fixed ts.config, tailwind.config and jest.config files for this project

---
+++ client/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  clearMocks: true,
+  verbose: true,
+  moduleFileExtensions: ['ts', 'js','mjs'],
+  moduleNameMapper: {
+    '.html$': '<rootDir>/../jest/HtmlStub.js',
+  },
+  rootDir: 'src/',
+  transform: {
+    "^.+\\.ts$": "ts-jest"
+  },
+  testRegex : "(/__tests__/.*|\\.(test|spec))\\.(ts|mjs)$",
+
+};
