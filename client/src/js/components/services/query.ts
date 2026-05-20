bf6547f5873b1fcc455648274c4a5f3c679ed90a
Eugen Morozov <26843480+meugenom@users.noreply.github.com>
2026-05-20 09:49:21 +0200

    added small fixes

--- client/src/js/components/services/query.ts
+++ client/src/js/components/services/query.ts
@@ -1,99 +1,97 @@
-import Config from '../../config';
+import Config from "../../config";
 
 //by development is default localhost will be used, but in production it will be taken from env variable or fallback to public host
 const Environment = {
-	host : Config.localHost
-}
+  host: Config.localHost,
+};
 
-if(process.env.APP_MODE == "production"){
-	Environment.host = process.env.GRAPHQL_HOST
+if (process.env.APP_MODE == "production") {
+  Environment.host = process.env.GRAPHQL_HOST;
 }
 
 const Query = {
-    lastArticlesList: {
-      host: Environment.host,
-      query: '{ lastArticlesList { slug title id date tags cluster order}}'
-    },
-    articlesList: {
-		host: Environment.host,
-      query: '{ articlesList { id title date slug tags cluster order}}'
-    },
-    tagsList: {
-      host: Environment.host,
-      query: '{ tagsList { name slug}}'
-    },
-    projectsList: {
-      host: 'https://api.github.com/graphql',
-      query: `{
-              user(login: "${process.env.GITHUB_USER_NAME}") {
-              email
-              location
-              websiteUrl
-              repositories
-              (last: 20,
-                  orderBy:
-                  {field: UPDATED_AT,
-                      direction: DESC
-                  })
-              {
-                  edges {
-                  node {
-                      id
-                      name
-                      description
-                      updatedAt
-					  pushedAt
-					  createdAt
-                      hasIssuesEnabled
-                      homepageUrl
-                      resourcePath
-                      openGraphImageUrl
-					  stargazers {
-						totalCount
-					  }
-					  forks {
-						totalCount
-					  }
-					  primaryLanguage {
-						name
-					  }
-					  languages(first: 6) {
-						nodes {
-						  name
-						}
-					  }
-                      repositoryTopics(first: 5) {
-                        edges {
-                          node {
-                            topic {
-                              name
-                            }
-                          }
-                        }
-                      }                      
-                  }
-                  }
+  lastArticlesList: {
+    host: Environment.host,
+    query: "{ lastArticlesList { slug title id date tags cluster order}}",
+  },
+  articlesList: {
+    host: Environment.host,
+    query: "{ articlesList { id title date slug tags cluster order}}",
+  },
+  tagsList: {
+    host: Environment.host,
+    query: "{ tagsList { name slug}}",
+  },
+  projectsList: {
+    host: "https://api.github.com/graphql",
+    query: `{
+    search(
+      query: "user:${process.env.GITHUB_USER_NAME} topic:portfolio",
+      type: REPOSITORY,
+      last: 20
+    ) {
+      edges {
+        node {
+          ... on Repository {
+            id
+            name
+            description
+            updatedAt
+            pushedAt
+            createdAt
+            hasIssuesEnabled
+            homepageUrl
+            resourcePath
+            openGraphImageUrl
+            stargazers {
+              totalCount
+            }
+            forks {
+              totalCount
+            }
+            primaryLanguage {
+              name
+            }
+            languages(first: 6) {
+              nodes {
+                name
               }
+            }
+            repositoryTopics(first: 7) {
+              edges {
+                node {
+                  topic {
+                    name
+                  }
+                }
               }
+            }
           }
-          `
-    },
-    getAllSpecificationTextByArticleId: {
-		  host: Environment.host,
-      query: 'query Query($id: Int!) {\n  getAllSpecificationTextByArticleId(id: $id) \n}\n'
-    },
-	  getAllSpecificationTextByArticleSlug: {
-		  host: Environment.host,
-		  query: 'query Query($slug: String!) {\n  getAllSpecificationTextByArticleSlug(slug: $slug) \n}\n'
-	  },
-    articlesListByTag: {
-      host: Environment.host,
-      query: 'query Query($tag: String!) {\n  articlesListByTag(tag: $tag){id\n title\n date\n tags\n slug\n cluster\n order\n} \n}\n'
-    },
-    searchArticles: {
-      host: Environment.host,
-      query: 'query Query($term: String!) {\n  searchArticles(term: $term){ id title slug date tags cluster order }\n}\n'
-    },
-  }
+        }
+      }
+    }
+  }`,
+  },
+  getAllSpecificationTextByArticleId: {
+    host: Environment.host,
+    query:
+      "query Query($id: Int!) {\n  getAllSpecificationTextByArticleId(id: $id) \n}\n",
+  },
+  getAllSpecificationTextByArticleSlug: {
+    host: Environment.host,
+    query:
+      "query Query($slug: String!) {\n  getAllSpecificationTextByArticleSlug(slug: $slug) \n}\n",
+  },
+  articlesListByTag: {
+    host: Environment.host,
+    query:
+      "query Query($tag: String!) {\n  articlesListByTag(tag: $tag){id\n title\n date\n tags\n slug\n cluster\n order\n} \n}\n",
+  },
+  searchArticles: {
+    host: Environment.host,
+    query:
+      "query Query($term: String!) {\n  searchArticles(term: $term){ id title slug date tags cluster order }\n}\n",
+  },
+};
 
-export default Query
+export default Query;
